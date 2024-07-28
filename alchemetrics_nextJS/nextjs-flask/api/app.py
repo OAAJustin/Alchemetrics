a634220c23d39103f336f46bdea53011422724f8
OAAJustin <justin.arroyooaa@gmail.com>
2024-07-28 18:26:34 -0400

    Adjust app.py SQL operation

--- alchemetrics_nextJS/nextjs-flask/api/app.py
+++ alchemetrics_nextJS/nextjs-flask/api/app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from mysql.connector import Error, cursor, connect
-from sqlalchemy import create_engine, text
 import json
-import pandas as pd
 
 app = Flask(__name__)
 
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
-
-engine = create_engine(f"mysql+mysqlconnector://{config['user']}:{config['password']}@{config['host']}/{config['database']}")
 
 @app.route('/add-item', methods=['POST'])
 def add_item():
@@ -33,16 +29,14 @@ def add_item():
         if connection.is_connected():
             print('Connected to the database')
 
-            # Your SQL operations go here
-            # For example, creating a cursor and executing a query
-
-        # Insert data into the MySQL table
-        with engine.connect() as connection:
-            query = text(f"""
+            # Insert data into the MySQL table
+            cursor = connection.cursor()
+            query = """
                 INSERT INTO Inventory (Title, Medium, Size, Qty, Price, Artist)
-                VALUES (title, medium, size, qty, price, artist)
-            """)
-            connection.execute(query, title=title, medium=medium, size=size, qty=qty, price=price, artist=artist)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """
+            
+            cursor.execute(query, (title, medium, size, qty, price, artist))
         
         return jsonify({'status': 'success', 'message': 'Item added and datbase connected!'}), 200
 
@@ -56,11 +50,6 @@ def add_item():
             cursor.close()
             connection.close()
             print("MySQL connection is closed")
-    
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
+      
 if __name__ == '__main__':
     app.run(debug = True, port = 3000)
