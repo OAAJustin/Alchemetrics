d210487bf5fe119a97c911e21e817459405948e0
tarroyo0907 <tarroyo0907@gmail.com>
2024-08-07 18:46:21 -0400

    Changed import_inventory

--- alchemetrics_nextJS/nextjs-flask/MySQL 8.0.32-1.2.11-server.session.sql
+++ alchemetrics_nextJS/nextjs-flask/MySQL 8.0.32-1.2.11-server.session.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-CREATE TABLE Inventory (UID INT PRIMARY KEY, Title TEXT, Medium TEXT, Size INT, Qty INT, Price FLOAT, Artist TEXT)
-=======
-SELECT * FROM Inventory
->>>>>>> 578e7e6db135486e8abfa5296f7db3aee71e3055
+CREATE TABLE Inventory (UID INT AUTO_INCREMENT PRIMARY Key, Artist Text, Title Text, Medium Text, Size INT, Qty INT, Price FLOAT)
